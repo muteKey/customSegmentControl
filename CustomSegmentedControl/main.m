01841030dfb8f62cf1df52b44015b906a73ee0ce
Kirill Ushkov <kirill.u@itomy.ch>
2016-03-30 16:15:22 +0300

    Initial Commit

---
+++ CustomSegmentedControl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomSegmentedControl
+//
+//  Created by Kirill Ushkov on 30/03/16.
+//  Copyright © 2016 Kirill Ushkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
